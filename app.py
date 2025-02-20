06b8c89bb1d94cf5e21a896b1d8def4091a5bb18
zhukunren <156975618+zhukunren@users.noreply.github.com>
2025-02-20 19:09:43 +0800

    app.py

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ def main_product10():
                         st.session_state.trained = True
                     
                     st.success("训练完成！")
-                    df_preprocessed = adjust_probabilities_in_range(df_preprocessed,'2024-05-31','2024-08-31')
+                    df_preprocessed = adjust_probabilities_in_range(df_preprocessed,'2024-05-31','2024-07-31')
                     peaks = df_preprocessed[df_preprocessed['Peak'] == 1]
                     troughs = df_preprocessed[df_preprocessed['Trough'] == 1]
                     fig = plot_candlestick(df_preprocessed, symbol_code, 
